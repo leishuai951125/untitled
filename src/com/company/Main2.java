11ae21786f32ecead69bf16e4cb98dd25d97d569
leishuai <1411265409@qq.com>
2018-07-24 19:35:38 +0800

    1

---
+++ src/com/company/Main2.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by leishuai on 2018/7/10.
+ */
+public class Main2 {
+    public static void main(String []args){
+        System.out.println("Main2");
+    }
+}
---
+++ src/com/leishuai/A.java
@@ -0,0 +1,10 @@
+package com.leishuai;
+
+/**
+ * Created by leishuai on 2018/7/10.
+ */
+public class A {
+    public void show(){
+        System.out.print("nihao");
+    }
+}
